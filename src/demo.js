a6441ff3b1fc4b4b283a8f44d037c0d9e859a80f
warwickgrigg <warwick.grigg@gmail.com>
2020-08-19 19:55:44 +0100

    wip

--- src/demo.js
+++ src/demo.js
@@ -1,14 +1,9 @@
-# ezform - terse forms for React and other vdom
-
-## Example
-
-```
 import React from "react";
 import ReactDOM from "react-dom";
-import {ezForm} from "ezform";
+import { Field, Form, parseFormInputs, fromExtraTerse } from "ezform";
 const h = React.createElement;
-const { Field, Form } = ezForm(h, {idPrefix: "r-"});
-let f = Form({
+//const { Field, Form } = ezForm(h, {idPrefix: "r-"});
+let f = h(Form, {
   onSubmit: (inputs) => console.log(JSON.stringify({ inputs }, null, 2)),
   children: [
     { ezTitle: "Text input with value", value: "text value" },
@@ -20,27 +15,21 @@ let f = Form({
     {
       ezTitle: "Radios",
       type: "radio",
-      children: [{ label: "Apple"}, { label: "Pear", checked: true  }],
+      children: [{ label: "Apple" }, { label: "Pear", checked: true }],
     },
     {
       type: "select",
       ezTitle: "Select fruit",
       multiple: true,
       children: [
-        {label: "Apple", value: "apple" },
-        {label: "Pear"},
-        {label: "Passion fruit"},
+        { label: "Apple", value: "apple" },
+        { label: "Pear" },
+        { label: "Passion fruit" },
       ],
     },
-    {ezTitle:"Submit"}
+    { ezTitle: "Submit" },
   ]
-  .map((f, i) => Field({ ...f, key: i })),
+    //.map(fromExtraTerse)
+    .map((f, i) => Field({ ...f, key: i })),
 });
 ReactDOM.render(f, document.getElementById("react"));
-```
-
-## API
-
-Form({onSubmit, children, ...props}) - component, returns form wrapping children
-
-Field({ezTitle, ...props} - component, returns element containing form group (eg div with label and input)
--- src/ezform.js
+++ src/ezform.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+const h = React.createElement;
 const iClass = "custom-control-input";
 const lClass = "custom-control-label";
 const gClass = "form-group";
@@ -47,139 +49,146 @@ const fromExtraTerse = (field) => {
  */
 
 /** @type {SharedLib.PriorityQueueFactory} */
-const ezForm = (h, { idPrefix = "" } = {}) => {
-  const idP = idPrefix;
-  const FormGroup = ({ children, ...props }) =>
-    h("div", { className: gClass, ...props }, ...children);
+//const ezForm = (h, { idPrefix = "" } = {}) => {
+//const idP = idPrefix;
+const FormGroup = ({ children, ...props }) =>
+  h("div", { className: gClass, ...props }, ...children);
 
-  const TickBox = ({ type, name, id, label, value = label, ...props }) => {
-    const className = `custom-control custom-${type} custom-control-inline`;
-    return h(
-      "div",
-      { className, key: id },
-      h("input", { id, name, className: iClass, type, value, ...props }),
-      h("label", { htmlFor: id, className: lClass }, label)
-    );
-  };
-
-  const TickBoxes = ({ type, name, key, label, children, ...props }) => {
-    type = type || "checkbox";
-    const xChildren = children.map((cProps, i) =>
-      TickBox({ type, name, ...props, id: idP + name + "_" + i, ...cProps })
-    );
-
-    return FormGroup({
-      key,
-      children: [
-        label && h("label", null, label),
-        h("div", null, ...xChildren),
-      ],
-    });
-  };
-
-  const SelectBox = ({ name, key, label, children, ...props }) => {
-    delete props.type;
-    const xChildren = children.map(({ label, value = label, ...cProps }, i) => {
-      const id = idP + name + "_" + i;
-      return h("option", { id, key: id, value, ...cProps }, label);
-    });
-
-    return FormGroup({
-      key,
-      children: [
-        label && h("label", { htmlFor: props.id }, label),
-        h("select", { name, ...props }, ...xChildren),
-      ],
-    });
-  };
-
-  const inferType = (name) => {
-    name = name.toLowerCase();
-    const types = ["number", "email", "textarea", "submit"];
-    return types.find((v) => name.includes(v)) || "text";
-  };
-  /** This is a description of the Text function. */
-  const Text = ({ name, type = inferType(name), key, label, ...props }) => {
-    //type = type || inferType(name);
-    const children =
-      type === "submit"
-        ? [
-            h("input", {
-              type,
-              name,
-              ...props,
-              value: label,
-              className: bClass,
-            }),
-          ]
-        : [
-            label && h("label", { htmlFor: props.id }, label),
-            type === "textarea"
-              ? h("textarea", { name, ...omit(["value"])(props) }, props.value)
-              : h("input", { type, name, ...props }),
-          ];
-    return FormGroup({ key, children });
-  };
-
-  /**
-   * High level user input field inc optional label, multiple options etc
-   * @function ezField~Field
-   * @param {Object} f field proerties
-   * @param {Object} [f.ezState] container for properties for intermediate values
-   * @param {string} [f.ezTitle] meaningful title for field
-   * @param {string} [f.type] html input type plus "select-multiple"
-   * @param {string[]} [f.options] options for checkbox, radio, select and select-multiple types
-   * @param {string} [f.label = f.ezTitle] label element
-   * @param {string} [f.placeholder = f.ezTitle] placeholder property
-   * @param {string} [f.name] name property - defaults to ezTitle or placeholder snake-cased
-   * @return {Object} vnode with optional label
-   *
-   * @example Field({placeholder: email})
-   *
-   */
-  const Field = ({
-    ezTitle,
-    ezState,
-    label = ezTitle,
-    placeholder = ezTitle,
-    name = (ezTitle || placeholder).replace(/ /g, "_").toLowerCase(),
-    id = idP + name,
-    key = id,
-    children,
-    ...props
-  }) => {
-    if (ezState && !props.value) props.value = ezState[name] || "";
-    const className = cClass;
-    if (children) {
-      return props.type === "select"
-        ? SelectBox({ label, key, id, name, children, className, ...props })
-        : TickBoxes({ label, key, id, name, children, ...props });
-    } else {
-      return Text({ label, key, id, name, placeholder, className, ...props });
-    }
-  };
-
-  /**
-   * Creates form element
-   * @param {function} onSubmit callback
-   * @return {string} A good string
-   *
-   * @example
-   *
-   *     <Form onSubmit={({email}) => console.log({email})}>
-   *        <Field
-   */
-
-  const Form = ({ onSubmit, children, ...props }) => {
-    if (onSubmit)
-      props.onSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(parseFormInputs(e.target));
-      };
-    return h("form", props, ...children);
-  };
-
-  return { Field, Form };
+const TickBox = ({ type, name, id, label, value = label, ...props }) => {
+  const className = `custom-control custom-${type} custom-control-inline`;
+  return h(
+    "div",
+    { className, key: id },
+    h("input", { id, name, className: iClass, type, value, ...props }),
+    h("label", { htmlFor: id, className: lClass }, label)
+  );
 };
 
-export { ezForm, fromExtraTerse, parseFormInputs };
+const TickBoxes = ({ type, name, idP, key, label, children, ...props }) => {
+  type = type || "checkbox";
+  const xChildren = children.map((cProps, i) =>
+    TickBox({ type, name, ...props, id: idP + name + "_" + i, ...cProps })
+  );
+
+  return FormGroup({
+    key,
+    children: [label && h("label", null, label), h("div", null, ...xChildren)],
+  });
+};
+
+const SelectBox = ({ name, idP, key, label, children, ...props }) => {
+  delete props.type;
+  const xChildren = children.map(({ label, value = label, ...cProps }, i) => {
+    const id = idP + name + "_" + i;
+    return h("option", { id, key: id, value, ...cProps }, label);
+  });
+
+  return FormGroup({
+    key,
+    children: [
+      label && h("label", { htmlFor: props.id }, label),
+      h("select", { name, ...props }, ...xChildren),
+    ],
+  });
+};
+
+const inferType = (name) => {
+  name = name.toLowerCase();
+  const types = ["number", "email", "textarea", "submit"];
+  return types.find((v) => name.includes(v)) || "text";
+};
+/** This is a description of the Text function. */
+const Text = ({ name, type = inferType(name), key, label, ...props }) => {
+  //type = type || inferType(name);
+  const children =
+    type === "submit"
+      ? [
+          h("input", {
+            type,
+            name,
+            ...props,
+            value: label,
+            className: bClass,
+          }),
+        ]
+      : [
+          label && h("label", { htmlFor: props.id }, label),
+          type === "textarea"
+            ? h("textarea", { name, ...omit(["value"])(props) }, props.value)
+            : h("input", { type, name, ...props }),
+        ];
+  return FormGroup({ key, children });
+};
+
+/**
+ * High level user input field inc optional label, multiple options etc
+ * @function ezField~Field
+ * @param {Object} f field proerties
+ * @param {Object} [f.ezState] container for properties for intermediate values
+ * @param {string} [f.ezTitle] meaningful title for field
+ * @param {string} [f.type] html input type plus "select-multiple"
+ * @param {string[]} [f.options] options for checkbox, radio, select and select-multiple types
+ * @param {string} [f.label = f.ezTitle] label element
+ * @param {string} [f.placeholder = f.ezTitle] placeholder property
+ * @param {string} [f.name] name property - defaults to ezTitle or placeholder snake-cased
+ * @return {Object} vnode with optional label
+ *
+ * @example Field({placeholder: email})
+ *
+ */
+const Field = ({
+  ezTitle,
+  ezState,
+  label = ezTitle,
+  placeholder = ezTitle,
+  name = (ezTitle || placeholder).replace(/ /g, "_").toLowerCase(),
+  idP = "",
+  id = idP + name,
+  key = id,
+  children,
+  ...props
+}) => {
+  if (ezState && !props.value) props.value = ezState[name] || "";
+  const className = cClass;
+  const derivedProps = { ...props, label, key, id, name, idP };
+  if (children) {
+    return props.type === "select"
+      ? SelectBox({ ...derivedProps, children, className })
+      : TickBoxes({ ...derivedProps, children });
+  } else {
+    return Text({ ...derivedProps, placeholder, className });
+  }
+};
+
+/**
+ * Creates form element
+ * @param {function} onSubmit callback
+ * @return {string} A good string
+ *
+ * @example
+ *
+ *     <Form onSubmit={({email}) => console.log({email})}>
+ *        <Field
+ */
+
+const Form = ({ onSubmit, ...props }) => {
+  const [values, setValues] = useState({});
+  const onChange = ({ target: { name, value } }) => (values) => {
+    console.log({ name, value });
+    return { ...values, [name]: value };
+  };
+  const clonEl = React.cloneElement;
+  const style = { background: "red" };
+  //const withOnChange = (e) => clonEl(e, { onChange, style });
+  const withValue = (e) => clonEl(e, { value: values. });
+  if (onSubmit)
+    props.onSubmit = (e) => {
+      e.preventDefault();
+      onSubmit(parseFormInputs(e.target));
+    };
+
+  return h("form", { ...props, onChange });
+  //children: children.map(withOnChange),
+};
+
+export { Field, Form, fromExtraTerse, parseFormInputs };
